52ba81db99bca236886f194d7eba4dfa33a73885
Prof. Antonio Adinolfi <antonioadinolfi@lscortese.com>
2026-01-10 17:16:45 +0100

    Create sicurezza.js

---
+++ sicurezza.js
@@ -0,0 +1,13 @@
+function controllaAccesso() {
+    const passInserita = document.getElementById('pass-docente').value;
+    
+    // Questa è la parola "adinolfi2026" codificata in Base64
+    const passSegreta = "YWRpbm9sZmkyMDI2"; 
+
+    if (btoa(passInserita) === passSegreta) {
+        document.getElementById('schermata-manutenzione').style.display = 'none';
+        sessionStorage.setItem('ok', 'true');
+    } else {
+        alert("Password errata!");
+    }
+}
